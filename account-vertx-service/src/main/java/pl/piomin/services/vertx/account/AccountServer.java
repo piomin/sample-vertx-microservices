7451b3b627b2d3d88c88913fd46289ddba0f4948
Piotr Mińkowski <piotr_minkowski@wp.pl>
2017-08-20 23:13:40 +0200

    fixes

--- account-vertx-service/src/main/java/pl/piomin/services/vertx/account/AccountServer.java
+++ account-vertx-service/src/main/java/pl/piomin/services/vertx/account/AccountServer.java
@@ -1,9 +1,16 @@
 package pl.piomin.services.vertx.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.LoggerFormat;
+import io.vertx.ext.web.handler.LoggerHandler;
 import io.vertx.ext.web.handler.ResponseContentTypeHandler;
 import pl.piomin.services.vertx.account.model.Account;
 
@@ -19,8 +26,22 @@ public class AccountServer extends AbstractVerticle {
 	public void start() throws Exception {
 		Router router = Router.router(vertx);
 		router.route("/account/*").handler(ResponseContentTypeHandler.create());
+		router.route("/account/*").handler(LoggerHandler.create());
+		router.route(HttpMethod.POST, "/account").handler(BodyHandler.create());
 		router.get("/account/:id").produces("application/json").handler(rc -> {
-			  rc.response().end(Json.encodePrettily(new Account(Integer.valueOf(rc.request().getParam("id")), "1234567", 1000, 2)));
+			rc.response().end(Json.encodePrettily(new Account(Integer.valueOf(rc.request().getParam("id")), "1234567", 1000, 2)));
+		});
+		router.get("/account").produces("application/json").handler(rc -> {
+			List<Account> accounts = new ArrayList<>();
+			accounts.add(new Account(1, "1234567", 1000, 2));
+			accounts.add(new Account(2, "1234568", 1300, 3));
+			accounts.add(new Account(3, "1234569", 700, 4));
+			rc.response().end(Json.encodePrettily(accounts));
+		});
+		router.post("/account").produces("application/json").handler(rc -> {
+			Account a = Json.decodeValue(rc.getBodyAsString(), Account.class);
+			a.setId(1);
+			rc.response().end(Json.encodePrettily(a));
 		});
 		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
 	}
