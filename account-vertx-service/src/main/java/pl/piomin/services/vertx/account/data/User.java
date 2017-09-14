7d6bf6bb4eb93c26374805cc9c973633aa955d17
piotr.minkowski <piotr.minkowski@gmail.com>
2017-09-14 15:52:27 +0200

    oauth2 changes

---
+++ account-vertx-service/src/main/java/pl/piomin/services/vertx/account/data/User.java
@@ -0,0 +1,62 @@
+package pl.piomin.services.vertx.account.data;
+
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+
+public class User {
+
+	private String accessToken;
+	private String username;
+	private String password;
+	private String scope;
+
+	public User() {
+
+	}
+
+	public User(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getScope() {
+		return scope;
+	}
+
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+
+	public JsonObject toJson() {
+		return JsonObject.mapFrom(this);
+	}
+	
+	@Override
+	public String toString() {
+		return Json.encodePrettily(this);
+	}
+
+}
