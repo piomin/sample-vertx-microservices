1ea7bdc38752a2226194d89b31277319f800ad02
piomin <piotr.minkowski@gmail.com>
2023-09-06 16:14:16 +0200

    add tests with testcontainers

---
+++ account-vertx-service/src/test/java/pl/piomin/services/vertx/account/AccountServerTests.java
@@ -0,0 +1,36 @@
+package pl.piomin.services.vertx.account;
+
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testcontainers.consul.ConsulContainer;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@ExtendWith(VertxExtension.class)
+public class AccountServerTests {
+
+    final static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:5.0"));
+    final static ConsulContainer consulContainer = new ConsulContainer("consul:1.14")
+            .withConsulCommand("kv put config/account-service test=abc");
+
+    @BeforeAll
+    static void init() {
+        mongoDBContainer.start();
+        consulContainer.start();
+    }
+
+    @AfterAll
+    static void destroy() {
+        mongoDBContainer.stop();
+        consulContainer.stop();
+    }
+
+    @Test
+    void startup() {
+
+    }
+}
